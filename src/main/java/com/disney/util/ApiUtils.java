dc2a8061432f991861803394a71f7b0c7d150683
Héctor Cortez <updown.input@gmail.com>
2023-11-01 23:24:04 -0300

    New: utility class

---
+++ src/main/java/com/disney/util/ApiUtils.java
@@ -0,0 +1,7 @@
+package com.disney.util;
+
+import java.time.format.DateTimeFormatter;
+
+public class ApiUtils {
+    public static final DateTimeFormatter OF_PATTERN = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+}
