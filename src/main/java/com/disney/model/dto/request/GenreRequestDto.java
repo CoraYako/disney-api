01d86caa4031f4749cb700fc5ab3bec41df25259
Héctor Cortez <updown.input@gmail.com>
2023-11-01 22:46:42 -0300

    New: deleted unnecessary attribute

--- src/main/java/com/disney/model/dto/request/GenreRequestDto.java
+++ src/main/java/com/disney/model/dto/request/GenreRequestDto.java
@@ -6,9 +6,6 @@ import jakarta.validation.constraints.NotEmpty;
 public record GenreRequestDto(
         @NotBlank(message = "The name can't be whitespaces")
         @NotEmpty(message = "The name can't be empty or null")
-        String name,
-        @NotBlank(message = "The image can't be whitespaces")
-        @NotEmpty(message = "The image can't be empty or null")
-        String image
+        String name
 ) {
 }
