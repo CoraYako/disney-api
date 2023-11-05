a5cd097ae884bcb578e6772f22614bc1a5aff89d
Héctor Cortez <updown.input@gmail.com>
2023-11-05 13:06:02 -0300

    Fix: unnecessary and unused classes deleted

--- src/main/java/com/disney/model/response/basic/CharacterBasicResponseList.java
+++
@@ -1,9 +0,0 @@
-package com.disney.model.response.basic;
-
-import java.util.List;
-
-
-public class CharacterBasicResponseList {
-
-    List<CharacterBasicResponse> characters;
-}
--- src/main/java/com/disney/model/response/basic/MovieBasicResponseList.java
+++
@@ -1,9 +0,0 @@
-package com.disney.model.response.basic;
-
-import java.util.List;
-
-
-public class MovieBasicResponseList {
-
-    private List<MovieBasicResponse> movies;
-}
