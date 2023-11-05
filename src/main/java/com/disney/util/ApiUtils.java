8b4f1566ba35f4e709cd0f2bfc38e89a215b5bd4
Héctor Cortez <updown.input@gmail.com>
2023-11-05 12:09:26 -0300

    New: method to be implemented in the Specifications to ascending or descending sorting

--- src/main/java/com/disney/util/ApiUtils.java
+++ src/main/java/com/disney/util/ApiUtils.java
@@ -4,4 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class ApiUtils {
     public static final DateTimeFormatter OF_PATTERN = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    public static boolean isASC(String order) {
+        return order.compareToIgnoreCase("ASC") == 0;
+    }
 }
