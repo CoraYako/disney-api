8285b577645a4369cf1bebb29c7fe073d16a0bbb
Héctor Cortez <updown.input@gmail.com>
2023-11-07 21:54:02 -0300

    New: method to obtain a valid UUID from a string

--- src/main/java/com/disney/util/ApiUtils.java
+++ src/main/java/com/disney/util/ApiUtils.java
@@ -1,10 +1,22 @@
 package com.disney.util;
 
+import com.disney.model.InvalidUUIDFormatException;
+
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class ApiUtils {
     public static final DateTimeFormatter OF_PATTERN = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
     public static boolean isASC(String order) {
         return order.compareToIgnoreCase("ASC") == 0;
+    }
+
+    public static UUID getUUIDFromString(String value) {
+        try {
+            return UUID.fromString(value);
+        } catch (Exception e) {
+            throw new InvalidUUIDFormatException(e.getMessage());
+        }
     }
 }
