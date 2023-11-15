3b12ffab66ae5b254859c7d3337f94d88d571743
Héctor Cortez <updown.input@gmail.com>
2023-11-15 10:43:58 -0300

    Feat: constants for genre url and times invoked for tests

--- src/main/java/com/disney/util/ApiUtils.java
+++ src/main/java/com/disney/util/ApiUtils.java
@@ -8,12 +8,14 @@ import java.util.UUID;
 
 public class ApiUtils {
     public static final DateTimeFormatter OF_PATTERN = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    public static final String GENRE_BASE_URL = "/api/v1/genres";
+    public static final String GENRE_URI_VARIABLE = "/{genreId}";
+    public static final int ELEMENTS_PER_PAGE = 10;
+    public static final int INVOKED_ONE_TIME = 1;
 
     public static boolean isASC(String order) {
         return order.compareToIgnoreCase("ASC") == 0;
     }
-
-    public static final int ELEMENTS_PER_PAGE = 10;
 
     public static UUID getUUIDFromString(String value) {
         try {
