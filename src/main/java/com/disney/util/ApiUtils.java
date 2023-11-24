98f3e09232e8c7e6cc6e2ecced75732564fbc73d
Héctor Cortez <updown.input@gmail.com>
2023-11-23 21:26:00 -0300

    Fix: deleted unnecessary int constant, added urls as constants

--- src/main/java/com/disney/util/ApiUtils.java
+++ src/main/java/com/disney/util/ApiUtils.java
@@ -10,8 +10,9 @@ public class ApiUtils {
     public static final DateTimeFormatter OF_PATTERN = DateTimeFormatter.ofPattern("yyyy/MM/dd");
     public static final String GENRE_BASE_URL = "/api/v1/genres";
     public static final String GENRE_URI_VARIABLE = "/{genreId}";
+    public static final String CHARACTER_BASE_URL = "/api/v1/characters";
+    public static final String CHARACTER_URI_VARIABLE = "/{characterId}";
     public static final int ELEMENTS_PER_PAGE = 10;
-    public static final int INVOKED_ONE_TIME = 1;
 
     public static boolean isASC(String order) {
         return order.compareToIgnoreCase("ASC") == 0;
